b87f2ebaa0f6554a24a59d14eaa1ac1e085f22d4
Жогин Евгений <zhogin02@mail.ru>
2022-06-06 19:17:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,116 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import top.defaults.colorpicker.ColorPickerPopup;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView gfgTextView;
+
+    // two buttons to open color picker dialog and one to
+    // set the color for GFG text
+    private Button mSetColorButton, mPickColorButton;
+
+    // view box to preview the selected color
+    private View mColorPreview;
+
+    // this is the default color of the preview box
+    private int mDefaultColor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gfgTextView = findViewById(R.id.gfg_heading);
+
+        // register two of the buttons with their
+        // appropriate IDs
+        mPickColorButton = findViewById(R.id.pick_color_button);
+        mSetColorButton = findViewById(R.id.set_color_button);
+
+        // and also register the view which shows the
+        // preview of the color chosen by the user
+        mColorPreview = findViewById(R.id.preview_selected_color);
+
+        // set the default color to 0 as it is black
+        mDefaultColor = 0;
+
+        mPickColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+
+                        new ColorPickerPopup.Builder(MainActivity.this).initialColor(
+                                Color.RED) // set initial color
+                                // of the color
+                                // picker dialog
+                                .enableBrightness(
+                                        true) // enable color brightness
+                                // slider or not
+                                .enableAlpha(
+                                        true) // enable color alpha
+                                // changer on slider or
+                                // not
+                                .okTitle(
+                                        "Choose") // this is top right
+                                // Choose button
+                                .cancelTitle(
+                                        "Cancel") // this is top left
+                                // Cancel button which
+                                // closes the
+                                .showIndicator(
+                                        true) // this is the small box
+                                // which shows the chosen
+                                // color by user at the
+                                // bottom of the cancel
+                                // button
+                                .showValue(
+                                        true) // this is the value which
+                                // shows the selected
+                                // color hex code
+                                // the above all values can be made
+                                // false to disable them on the
+                                // color picker dialog.
+                                .build()
+                                .show(
+                                        v,
+                                        new ColorPickerPopup.ColorPickerObserver() {
+                                            @Override
+                                            public void
+                                            onColorPicked(int color) {
+                                                // set the color
+                                                // which is returned
+                                                // by the color
+                                                // picker
+                                                mDefaultColor = color;
+
+                                                // now as soon as
+                                                // the dialog closes
+                                                // set the preview
+                                                // box to returned
+                                                // color
+                                                mColorPreview.setBackgroundColor(mDefaultColor);
+                                            }
+                                        });
+                    }
+                });
+
+        mSetColorButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gfgTextView.setTextColor(mDefaultColor);
+                    }
+                }
+        );
+    }
+}
